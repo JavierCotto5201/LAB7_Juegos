471e4a3bbecc43e98325c14e5250f5b7e1f8a515
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2020-05-12 23:36:24 -0600

    Primer Commit
    Bases del laboratorio

---
+++ CottoJavier_LAB7/Assets/Scripts/Manager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject prefab;
+    private GameObject newObj;
+
+    public GameObject pauseMenu;
+    private bool isPaused = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+
+        if (prefab)
+            newObj = Instantiate(prefab, new Vector3(0, 3, 0), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            TogglePause();
+
+        if(Input.GetKeyDown(KeyCode.Return) && !newObj)
+            newObj = Instantiate(prefab, new Vector3(0, 3, 0), Quaternion.identity);
+
+    }
+
+    public void TogglePause() 
+    {
+
+        if (pauseMenu)
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            isPaused = !isPaused;
+            Time.timeScale = isPaused ? 1.0f : 0.0f;
+        }
+    
+    }
+
+    public void MenuInicio() {
+
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void IniciarJuego() {
+
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void RestartScene() 
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+}
---
+++ CottoJavier_LAB7/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody rb;
+
+    public float score = 0;
+    public Text scoreText;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+        scoreText = GameObject.FindGameObjectWithTag("ScoreText").GetComponent<Text>();
+
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(myRay, out hitInfo))
+            {
+
+                agent.SetDestination(hitInfo.point);
+
+            }
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {        
+        if (collision.gameObject.CompareTag("Destruccion"))
+            Destroy(gameObject);
+                    
+    }
+}
